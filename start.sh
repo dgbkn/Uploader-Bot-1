60d934c3f672dfa3dabdf98637e623b8aa92c587
_dev_goyal__ <65810521+dgbkn@users.noreply.github.com>
2022-05-12 16:47:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
 git clone https://github.com/dgbkn/Uploader-Bot-1
-cd /Uploader-Bot-1
+cd Uploader-Bot-1
 pip3 install -U -r requirements.txt
 echo "Starting Bot.... Please Wait. Check You Log. Bot Credit @DKBOTZ\n Special Credit :- @Tellybots"
 python3 bot.py
